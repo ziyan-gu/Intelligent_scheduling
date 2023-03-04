d7bb35b1cbcb3c740e797c68e7b77f2c7bbbb68c
wx <fantacy_7@163.com>
2023-03-04 23:32:38 +0800

    算法模块注册

--- Intelligent_scheduling_algorithm/src/main/java/com/backend/IntelligentSchedulingAlgorithmApplication.java
+++ Intelligent_scheduling_algorithm/src/main/java/com/backend/IntelligentSchedulingAlgorithmApplication.java
@@ -2,8 +2,10 @@ package com.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class IntelligentSchedulingAlgorithmApplication {
     public static void main(String[] args) {
         SpringApplication.run(IntelligentSchedulingAlgorithmApplication.class, args);
---
+++ Intelligent_scheduling_user_service/src/main/java/com/backend/intelligent_scheduling_login/feign/orderFeign.java
@@ -0,0 +1,11 @@
+package com.backend.intelligent_scheduling_login.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * 远程调用排班算法
+ */
+@FeignClient(value = "orderService")
+public interface orderFeign {
+
+}
