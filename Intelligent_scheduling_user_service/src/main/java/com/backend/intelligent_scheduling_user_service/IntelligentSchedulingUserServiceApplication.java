0e2291b47ff718edbf3c3ff5b07ddd9492f8618c
wx <fantacy_7@163.com>
2023-03-22 23:53:30 +0800

    启动类名

--- Intelligent_scheduling_user_service/src/main/java/com/backend/intelligent_scheduling_user_service/IntelligentSchedulingUserServiceApplication.java
+++ Intelligent_scheduling_user_service/src/main/java/com/backend/intelligent_scheduling_user_service/IntelligentSchedulingUserServiceApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class IntelligentSchedulingLoginApplication {
+public class IntelligentSchedulingUserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IntelligentSchedulingLoginApplication.class, args);
+        SpringApplication.run(IntelligentSchedulingUserServiceApplication.class, args);
     }
 
 }
